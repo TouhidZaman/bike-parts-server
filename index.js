672c3063a70bbbc3fa18ab992af8643eeccf66b8
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-05-22 21:17:50 +0600

    Middleware verifyJWT added to prevent unauthorized access, get all users api added and protected withverifyJWT

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//For Token verification
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers?.authorization;
+    if (!authHeader) {
+        return res
+            .status(401)
+            .send({ success: false, message: "Unauthorized Access" });
+    } else {
+        const token = authHeader.split(" ")[1];
+
+        // verify a token symmetric
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+            // console.log("decoded", decoded);
+            req.decoded = decoded;
+            next();
+        });
+    }
+};
+
 //MongoDB Config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.puprz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -52,6 +74,13 @@ async function run() {
             // console.log(result, accessToken);
             res.send({ result, accessToken });
         });
+
+        //Getting all registered users
+        app.get("/users", verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+        
     } finally {
         //   await client.close();
     }
