54708b00111d45e68715ff6ba7538fdaf9cbc906
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-05-22 20:42:06 +0600

    mongodb config, user insert with generate jsonwebtoken added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,6 +12,51 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//MongoDB Config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.puprz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+// API Endpoints
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("bikePartsDB").collection("users");
+
+        //Creating user and getting token for user
+        app.put("/users/:email", async (req, res) => {
+            const email = req.params?.email;
+            const user = req.body;
+
+            const filter = { email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const accessToken = jwt.sign(
+                { email },
+                process.env.ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: "1d",
+                }
+            );
+            // console.log(result, accessToken);
+            res.send({ result, accessToken });
+        });
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 //API Endpoints
 app.get("/", (req, res) => {
